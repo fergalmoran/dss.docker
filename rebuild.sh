4bdef0ae54120421da2a7eeb10d3f1846ee65a55
Fergal Moran <fergal.moran@gmail.com>
2015-10-05 19:15:19 +0000

    Pre merge

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,5 @@ export PATH=$PATH:/mnt/bin/
 git pull && \
 	cd dss.web && git pull && cd .. && \
 	cd dss.api && git pull && cd .. && \
-	cd dss.radio && git radio && cd .. && \
+	cd dss.radio && git pull && cd .. && \
 	docker-compose build && docker-compose up $1
