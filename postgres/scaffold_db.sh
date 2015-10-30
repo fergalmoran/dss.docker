a3074030348edf1bbe57affbed5adbca77aa26f1
Fergal Moran <fergal.moran@gmail.com>
2015-10-30 10:56:12 +0000

    Moved to images

--- postgres/scaffold_db.sh
+++ postgres/scaffold_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo -u postgres psql -h 127.0.0.1 -c "CREATE DATABASE deepsouthsounds"
 sudo -u postgres psql -h 127.0.0.1 -c "CREATE USER deepsouthsounds WITH PASSWORD 'deepsouthsounds'"
 sudo -u postgres psql -h 127.0.0.1 -c "CREATE DATABASE deepsouthsounds OWNER deepsouthsounds"
-sudo -u postgres psql -h 127.0.0.1 deepsouthsounds < /home/fergalm/Dropbox/dss.sql
+sudo -u postgres psql -h 127.0.0.1 deepsouthsounds < /tmp/20151025-203518.sql  
