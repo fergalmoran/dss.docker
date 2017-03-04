c296c2a005553348f0efe44b76473a4af820ffe3
Fergal Moran <fergal.moran@gmail.com>
2017-03-04 17:04:51 +0000

    Update scaffold_db.sh

--- postgres/scaffold_db.sh
+++ postgres/scaffold_db.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-sudo -u postgres psql -h 127.0.0.1 -c "CREATE DATABASE deepsouthsounds"
-sudo -u postgres psql -h 127.0.0.1 -c "CREATE USER deepsouthsounds WITH PASSWORD 'deepsouthsounds'"
-sudo -u postgres psql -h 127.0.0.1 -c "CREATE DATABASE deepsouthsounds OWNER deepsouthsounds"
-sudo -u postgres psql -h 127.0.0.1 deepsouthsounds < /tmp/20151025-203518.sql  
+su - postgres
+psql -h 127.0.0.1 -c "CREATE USER deepsouthsounds WITH PASSWORD 'deepsouthsounds'"
+psql -h 127.0.0.1 -c "CREATE DATABASE deepsouthsounds OWNER deepsouthsounds"
+psql -h 127.0.0.1 deepsouthsounds < /tmp/20151025-203518.sql  
